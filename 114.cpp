7dabe6848003c2e4c5089a29c262491b273e521d
Derdzyan2005 <a.derdzyan2005@gmail.com>
2024-11-14 11:42:00 +0400

    Create 114.cpp

---
+++ 114.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if (!root) return;
+        
+        std::stack<TreeNode*> stack;
+        stack.push(root);
+        
+        while (!stack.empty()) {
+            TreeNode* current = stack.top();
+            stack.pop();
+            
+            if (current->right) {
+                stack.push(current->right);
+            }
+            if (current->left) {
+                stack.push(current->left);
+            }
+            
+            if (!stack.empty()) {
+                current->right = stack.top();
+            }
+            current->left = nullptr;
+        }
+    }
+};
