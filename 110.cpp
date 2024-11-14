674ea87553f3f8f229446555dc49e32b916511f7
Derdzyan2005 <a.derdzyan2005@gmail.com>
2024-11-14 11:41:32 +0400

    Create 110.cpp

---
+++ 110.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        return height(root) != -1;
+    }
+    
+private:
+    int height(TreeNode* node) {
+        if (!node) return 0;
+        int leftHeight = height(node->left);
+        int rightHeight = height(node->right);
+        if (leftHeight == -1 || rightHeight == -1 || abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+        return max(leftHeight, rightHeight) + 1;
+    }
+};
